e5a006f8d90cd12849a7ce4b41437b174245a0e5
sai <alli.yadav@yahoo.in>
2024-08-28 12:47:44 +0000

    Demo push

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWrold{
   public static void main(String args[]){
     System.out.println("Hello World!");
     System.out.println("Second line to demo pull");
+    System.out.println("Change made from allis computer to demo push");
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # git_demo
 This repository is created to demo git
 Pull demo
+Push Demo
